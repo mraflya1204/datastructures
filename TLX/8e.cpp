c76977e5e2ae38bf58338312d8738e81aaddda01
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2024-03-04 22:35:50 +0700

    Update 8e.cpp

--- TLX/8e.cpp
+++ TLX/8e.cpp
@@ -6,8 +6,17 @@
 int main(){
     int N; scanf("%d", &N);
     int arr[N];
+    int counter = 0;
 
     for(int i = 0; i < N; i++){
         scanf("%d\n", &arr[i]);
     }
+
+    for(int i = 0; i < N-1; i++){
+        if(arr[i] >= arr[i+1]){
+            counter += arr[i];
+        }
+        else counter += arr[i+1];
+    }
+    printf("%d\n", counter);
 }
