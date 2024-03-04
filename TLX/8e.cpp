a33a9d56b317f37d8c11e87ed08f8da961505068
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2024-03-04 17:27:29 +0700

    Add files via upload

---
+++ TLX/8e.cpp
@@ -0,0 +1,13 @@
+//Made by Muhammad Rafly Abdillah @DataStructuresIUP2024
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int N; scanf("%d", &N);
+    int arr[N];
+
+    for(int i = 0; i < N; i++){
+        scanf("%d\n", &arr[i]);
+    }
+}
